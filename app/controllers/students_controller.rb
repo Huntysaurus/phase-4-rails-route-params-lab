8fe3bcef745959cd09c09a1de657809711ac70f6
Jacob Yaw <yawjacob@yahoo.com>
2022-06-24 21:33:23 -0500

    commit tree 6185d3d8a2e8f44cc9b36a250b76ebb1cd83742c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,12 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
-    render json: students
+    # students = Student.all
+    # render json: students
+  end
+
+  def by_id
+    render json: Student.find(params[:id])
   end
 
 end
